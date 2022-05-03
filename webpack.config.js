63ca0f6ab433eaf1a4d4d17ceb86681c1a69d017
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-03 16:22:05 -0500

    chore: added loader configuration of babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     // Entry point is the modele that webpack uses to start building its internal dependency graph.
@@ -8,8 +9,23 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
-    // Define what's extensions webpack will work to
+    // Options for resolving module request. (Does not apply to resolving of loaders)
     resolve: {
         extensions: ['.js']
-    }
+    },
+    //Webpack only understands JS and JSON files. Loaders allow webpack to process other types of files and convert them into valid modules that can be consumed by your application and added to the dependency graph
+    module: {
+        rules: [
+            {
+                // test property identifies which file or files should be transform
+                test: /\.m?js$/, 
+                // Exclude all modules matching any of these conditions
+                exclude: /node_modules/,
+                // use property indicates which loader should be used to do the transform
+                use:{ 
+                    loader: 'babel-loader'
+                }
+            }
+        ]    
+    },
 }
