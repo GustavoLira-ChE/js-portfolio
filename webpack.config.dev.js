d4fc51b521ef67f21b780d8164b5e405bd65acbc
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-04 16:26:04 -0500

    chore: added config to webpack config dev to run server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -90,4 +90,11 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 8001,
+        open: true
+    }
 }
